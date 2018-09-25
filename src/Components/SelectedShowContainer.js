9ee71f3ce732eccd5467a13a0a3e864b7e4fb5d1
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-09-25 14:17:09 +0100

    jo

--- src/Components/SelectedShowContainer.js
+++ src/Components/SelectedShowContainer.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import Episode from './Components/Episode';
+import React, { Component } from 'react'
+import Episode from './Episode'
 
 class SelectedShowContainer extends Component {
-
   state = {
-    selectedSeason: 1,
+    selectedSeason: 1
   }
 
   mapSeasons = () => {
-    if (!!this.props.episodes){
-      let seasons = this.props.episodes.map((e)=> e.season).unique()
+    if (this.props.allEpisodes) {
+      let seasons = this.props.allEpisodes.map((e) => e.season).unique()
 
       return seasons.map((s) => {
         return (<option value={s} key={s}>Season {s}</option>)
-      });
+      })
     }
   }
 
   mapEpisodes = () => {
-    return this.props.episodes.map((e)=>{
-      if (e.season == this.state.selectedSeason){
-        return (<Episode eachEpisode={e} key={e.id}/>)
+    return this.props.allEpisodes.map((e) => {
+      if (e.season == this.state.selectedSeason) {
+        return (<Episode eachEpisode={e} key={e.id} />)
       }
     })
   }
@@ -29,37 +28,34 @@ class SelectedShowContainer extends Component {
     this.setState({ selectedSeason: e.target.value })
   }
 
-
-  render() {
+  render () {
     const { selectedShow } = this.props
-
+    console.log(this.props)
     return (
-      <div style={{position: "static"}}>
+      <div style={{ position: 'static' }}>
         <h2>{selectedShow.name}</h2>
-        <img src={selectedShow.image.medium} alt=""/>
-        <p dangerouslySetInnerHTML={{__html: selectedShow.summary}}></p>
+        <img src={selectedShow.image.medium} alt='' />
+        <p dangerouslySetInnerHTML={{ __html: selectedShow.summary }} />
         <p>Premiered: {selectedShow.premiered}</p>
         <p>Status: {selectedShow.status}</p>
         <p>Average Rating: {selectedShow.rating.average}</p>
-        <select style={{display: 'block'}} onChange={this.handleSelectionChange}>
+        <select style={{ display: 'block' }} onChange={this.handleSelectionChange}>
           {this.mapSeasons()}
         </select>
         {this.mapEpisodes()}
       </div>
-    );
+    )
   }
-
 }
 
-export SelectedShowContainer;
+export default SelectedShowContainer
 
-
-Array.prototype.unique = function() {
-  var arr = [];
-  for(var i = 0; i < this.length; i++) {
-    if(!arr.includes(this[i])) {
-        arr.push(this[i]);
+Array.prototype.unique = function () {
+  var arr = []
+  for (var i = 0; i < this.length; i++) {
+    if (!arr.includes(this[i])) {
+      arr.push(this[i])
     }
   }
-  return arr;
+  return arr
 }
